1d11158a28880dbcddecca494ee6c04bebc819cb
sckwave <67253854+sckwave@users.noreply.github.com>
2026-04-22 15:50:14 +0800

    Update ipq807x.h
    Signed-off-by: sckwave <67253854+sckwave@users.noreply.github.com>

--- include/configs/ipq807x.h
+++ include/configs/ipq807x.h
@@ -301,6 +301,7 @@ extern loff_t board_env_size;
 #define CONFIG_SYS_USB_XHCI_MAX_ROOT_PORTS      2
 #define CONFIG_USB_MAX_CONTROLLER_COUNT         2
 #endif
+#define CONFIG_CMD_FAT
 
 #define PCI_MAX_DEVICES	2
 #ifdef CONFIG_PCI_IPQ
